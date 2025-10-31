a3d392be7aa88c03580b5f8692083ece1dcaef9f
Shady <abdullashahidmasood@gmail.com>
2025-10-31 20:55:30 +0500

    Added unittest framework for future testing

---
+++ test_cafe.py
@@ -0,0 +1,18 @@
+from cafe import order,summ,slip,gq
+import unittest
+
+class TestCafe(unittest.TestCase):
+    def test_order():
+        ...
+
+    def test_summ():
+        ...
+	
+    def test_slip():
+        ...
+	
+    def test_gq():
+        ...
+        
+if __name__=="__main__":
+    unittest.main()
